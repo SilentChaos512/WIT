7f0f486870cdc8e0c228754be3c5d481d4bbdf3e
SilentChaos512 <sworkman256@gmail.com>
2019-02-10 15:09:19 -0600

    Cleanup main class, remove some unneeded files

--- src/main/java/net/silentchaos512/wit/WIT.java
+++ src/main/java/net/silentchaos512/wit/WIT.java
@@ -9,42 +9,8 @@ import org.apache.logging.log4j.Logger;
 public class Wit implements ModInitializer {
     public static final String MOD_ID = "wit";
     public static final String MOD_NAME = "Wit: Fabric Edition";
-    public static final String VERSION_NUMBER = "1.0.0";
 
     public static Logger LOGGER = LogManager.getLogger(MOD_NAME);
-
-    public boolean foundStorageDrawers = false;
-    public boolean foundMcMultiPart = false;
-
-    public static Wit INSTANCE;
-
-    public Wit() {
-        INSTANCE = this;
-//        Config.load();
-
-//        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, Wit::onRenderOverlay);
-    }
-
-/*    private static void onRenderOverlay(RenderGameOverlayEvent event) {
-        if (event.isCancelable() || event.getType() != RenderGameOverlayEvent.ElementType.TEXT) {
-            return;
-        }
-
-        GuiScreen screen = Minecraft.getInstance().currentScreen;
-        if (screen != null && !screen.doesGuiPauseGame() && Config.HUD.hideWhenGuiOpen.get()) {
-            return;
-        }
-
-        HudRenderObject renderObject = RayTraceHelper.getRenderObject(event.getPartialTicks());
-
-        if (renderObject != null) {
-            renderObject.render(event);
-        } else {
-            HudRenderObject.adjustBackgroundHeight(event, HudRenderObject.lastMaxBackgroundHeight, false);
-            HudRenderObject.renderBackground(HudRenderObject.lastMaxBackgroundWidth,
-                    HudRenderObject.lastBackgroundPosX, HudRenderObject.lastBackgroundPosY);
-        }
-    }*/
 
     @Override
     public void onInitialize() {
